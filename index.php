1c803987b4e9a72c74a66c2563498f8cbc3aa418
Gustavo Fuentes <gustavof@lagash.com>
2018-03-15 19:48:43 -0300

    -

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
  </head>
  <body>
  <?php echo '<p>Hola Mundo: GCBA</p>'; ?>
- <span>db-></span><?php echo getenv("db"); ?><span><-enddb</span>
+ <span>db-></span><?php echo getenv("db"); ?><span><-db</span>
+ <span>testenv-></span><?php echo getenv("testenv"); ?><span><-testenv</span>
  </body>
 </html>
